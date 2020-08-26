3b2cc43be7511651e4c15c83ea5a64d1c149a33b
anonyauthors <70259765+anonyauthors@users.noreply.github.com>
2020-08-26 15:46:09 +0200

    Update infer.py

--- code/1-encoder/run-on-test/infer.py
+++ code/1-encoder/run-on-test/infer.py
@@ -1,26 +1,25 @@
 import subprocess
 
-path_datayml = '../trainin/data.yml'
 
-# 1 encoder
-features_file = 
+#DATA
 
-path_tgt_test = 
+path_datayml = '../training/data.yml'
+src_file = '../../../datasets/1-encoder/test/src-test.txt'
+
 path_pred = 'predictions.txt'
 
 # INFER
 
-infer_command = 'onmt-main --config ' + path_datayml + ' --auto_config infer --features_file ' \
-       + features_file + ' --predictions_file ' + path_pred
+infer_command = 'onmt-main --config ' + path_datayml + ' --auto_config infer --src_file ' + features_file + ' --predictions_file ' + path_pred
 
-f = open(path_folder + 'infer.sh', 'w')
+f = open('./infer.sh', 'w')
 f.close()
 
 subprocess.run('chmod a+x infer.sh', shell=True)
 
-f = open(path_folder + 'infer.sh', 'w')
+f = open('./infer.sh', 'w')
 f.write('#!/usr/bin/env bash\n')
 f.write(infer_command)
 f.close()
 
-subprocess.run(path_folder + 'infer.sh')
+subprocess.run('./infer.sh')
