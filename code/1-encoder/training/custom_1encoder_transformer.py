6031d95acd628ed2042f9bfdc719eb708f00152f
anonyauthors <70259765+anonyauthors@users.noreply.github.com>
2020-08-28 16:38:29 +0200

    Update custom_1encoder_transformer.py

--- code/1-encoder/training/custom_1encoder_transformer.py
+++ code/1-encoder/training/custom_1encoder_transformer.py
@@ -6,13 +6,13 @@ class MyCustomTransformer(opennmt.models.Transformer):
         super().__init__(
             source_inputter=opennmt.inputters.WordEmbedder(256),
             target_inputter=opennmt.inputters.WordEmbedder(256),
-            num_layers=[1, 2],
+            num_layers=[1, 3],
             num_units=256,
             num_heads=8,
-            ffn_inner_dim=512,
-            dropout=0.319,
-            attention_dropout=0.3985,
-            ffn_dropout=0.0998)
+            ffn_inner_dim=256,
+            dropout=0.2798,
+            attention_dropout=0.1873,
+            ffn_dropout=0.2134)
 
 
 model = MyCustomTransformer
