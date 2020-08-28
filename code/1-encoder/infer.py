48730c65853b57a3f6fbeb74ee8949482d9d0d81
anonyauthors <70259765+anonyauthors@users.noreply.github.com>
2020-08-28 21:16:20 +0200

    Add files via upload

---
+++ code/1-encoder/infer.py
@@ -0,0 +1,24 @@
+import subprocess
+
+
+#DATA
+
+path_datayml = '../training/data.yml'
+src_file = '../../../datasets/1-encoder/test/src-test.txt'
+path_pred = 'predictions.txt'
+
+# INFER
+
+infer_command = 'onmt-main --config ' + path_datayml + ' --auto_config infer --features_file ' + src_file + ' --predictions_file ' + path_pred
+
+f = open('./infer.sh', 'w')
+f.close()
+
+subprocess.run('chmod a+x infer.sh', shell=True)
+
+f = open('./infer.sh', 'w')
+f.write('#!/usr/bin/env bash\n')
+f.write(infer_command)
+f.close()
+
+subprocess.run('./infer.sh')
