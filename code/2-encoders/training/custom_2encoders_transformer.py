fd4396ef91046417b4280460a74f9cfd830e9e4c
anonyauthors <70259765+anonyauthors@users.noreply.github.com>
2020-08-28 16:40:46 +0200

    Update custom_2encoders_transformer.py

--- code/2-encoders/training/custom_2encoders_transformer.py
+++ code/2-encoders/training/custom_2encoders_transformer.py
@@ -5,16 +5,16 @@ class MyCustomTransformer(opennmt.models.Transformer):
     def __init__(self):
         super().__init__(
             source_inputter=opennmt.inputters.ParallelInputter(
-                [opennmt.inputters.WordEmbedder(embedding_size=2048),
-                 opennmt.inputters.WordEmbedder(embedding_size=2048)]),
-            target_inputter=opennmt.inputters.WordEmbedder(embedding_size=2048),
-            num_layers=[4, 3],
-            num_units=2048,
-            num_heads=8,
-            ffn_inner_dim=256,
-            dropout=0.0341,
-            attention_dropout=0.2114,
-            ffn_dropout=0.0735,
+                [opennmt.inputters.WordEmbedder(embedding_size=512),
+                 opennmt.inputters.WordEmbedder(embedding_size=512)]),
+            target_inputter=opennmt.inputters.WordEmbedder(embedding_size=512),
+            num_layers=[2, 4],
+            num_units=512,
+            num_heads=4,
+            ffn_inner_dim=512,
+            dropout=0.1168,
+            attention_dropout=0.1794,
+            ffn_dropout=0.2809,
             share_encoders=True)
 
 
